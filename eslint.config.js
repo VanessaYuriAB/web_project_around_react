65eb7bdf7776988fa02173e19c32e52b6f4e51c9
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-02 10:06:41 -0300

    feat: remove .eslintrc.cjs, update eslint.config.js and add Prettier config for consistent code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -23,7 +23,16 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }], // ignora variáveis que começam com letra maiúscula ou são constantes
+
+      'react/jsx-no-target-blank': 'off', // desativa regra que impede uso de target="_blank" sem rel="noreferrer"
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ], // permite exportar componentes constantes sem aviso
+      'react/prop-types': 'off', // desativa validação de prop-types
+
+      quotes: ['error', 'single'], // força uso de aspas simples
     },
   },
-])
+]);
