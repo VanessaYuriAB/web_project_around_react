f74176b414ed97e6708a8667df78c64e938fbb50
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-03 14:12:59 -0300

    feat: add conditional logic to Popup for rendering ImagePopup

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -2,11 +2,16 @@ export default function Popup(props) {
   //children é o conteúdo de popup
   const { children, onClose } = props;
 
+  // Para verificar se children é o componente ImagePopup e configurar a classe do botão de fechar
+  const isImagePopup = children.type.name === 'ImagePopup';
+
   return (
     <div className="popup popup_closed">
       {children}
       <button
-        className="icon-close-btn popup__icon-close-btn"
+        className={`icon-close-btn popup__icon-close-btn ${
+          isImagePopup ? 'popup__icon-close-btn_card' : ''
+        }`}
         type="button"
         aria-label="Botão de fechar"
         onClick={onClose}
