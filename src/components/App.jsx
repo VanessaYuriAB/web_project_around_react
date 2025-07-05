2d2d3a2815330e3db9cafd71a7e1a37fd76fc1bb
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-05 18:10:32 -0300

    refactor: remove unused state management from App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
-
 import Header from './Header/Header.jsx';
 import Main from './Main/Main.jsx';
 import Footer from './Footer/Footer.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="page">
       <Header />
