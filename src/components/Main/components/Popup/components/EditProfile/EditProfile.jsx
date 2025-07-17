d0ac449d1f34367a9f784ca3a405ced6ff171f2f
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-17 06:58:01 -0300

    feat: add state management to EditProfile form with currentUser context defaults

--- src/components/Main/components/Popup/components/EditProfile/EditProfile.jsx
+++ src/components/Main/components/Popup/components/EditProfile/EditProfile.jsx
@@ -1,4 +1,22 @@
+import { useState, useContext } from 'react';
+import CurrentUserContext from '../../../../../../contexts/CurrentUserContext.js';
+
 function EditProfile() {
+  // Obtém o usuário atual do contexto: assina o contexto CurrentUserContext, permitindo que o componente acesse as informações do usuário atual, como nome e descrição
+  const currentUser = useContext(CurrentUserContext);
+
+  // Define o estado inicial do formulário com os valores do usuário atual, garantindo que o formulário seja preenchido com as informações corretas quando aberto
+  const [name, setName] = useState(currentUser.name); // adiciona variável de estado para nome e usa o nome do usuário atual como valor inicial do estado
+  const [about, setAbout] = useState(currentUser.about); // adiciona variável de estado para descrição e usa a descrição do usuário atual como valor inicial do estado
+
+  function handleNameChange(event) {
+    setName(event.target.value); // atualiza o nome (name) quando a entrada for alterada
+  }
+
+  function handleAboutChange(event) {
+    setAbout(event.target.value); // atualiza a descrição (about) quando a entrada for alterada
+  }
+
   return (
     <form
       className="popup__container_edt"
@@ -16,6 +34,8 @@ function EditProfile() {
         minLength="2"
         maxLength="40"
         required
+        value={name} // vincula nome ao campo de entrada, usando o valor do estado 'name' para preenchê-lo
+        onChange={handleNameChange}
       />
       <span
         className="popup__input-error_edt name-input-error"
@@ -30,6 +50,8 @@ function EditProfile() {
         minLength="2"
         maxLength="200"
         required
+        value={about} // vincula descrição ao campo de entrada, usando o valor do estado 'about' para preenchê-lo
+        onChange={handleAboutChange}
       />
       <span
         className="popup__input-error_edt about-input-error"
