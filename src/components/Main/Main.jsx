e46b3ad26ef3da3fa74fb146c15cd3fc9ab968a4
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-17 04:39:03 -0300

    feat: integrate CurrentUserContext to display user profile information

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,6 +10,8 @@ import Card from './components/Card/Card';
 
 import myApi from '../../utils/api.js';
 
+import CurrentUserContext from '../../contexts/CurrentUserContext.js';
+
 /*
 Linha comentada para prevenir duplicação ao enviar cards iniciais. Executar apenas uma vez, quando necessário enviar os dados para a API.
 // envia meus cards iniciais
@@ -20,6 +22,9 @@ function Main() {
   const [popup, setPopup] = useState(null);
 
   const [cards, setCards] = useState([]);
+
+  // Obtém o usuário atual do contexto: assina o contexto CurrentUserContext
+  const currentUser = useContext(CurrentUserContext);
 
   useEffect(() => {
     myApi
@@ -56,7 +61,15 @@ function Main() {
     <main className="content page__content">
       <section className="profile content__profile">
         <div className="photo profile__photo">
-          {/* foto configurada pelo css, como background-image */}
+          {/* foto configurada pelo css, como background-image, antes de conectar com as infos da API*/}
+          <img
+            src={currentUser.avatar}
+            alt="Foto do perfil"
+            className="profile__photo_img"
+          />
+
+          {/* Tag img adicionada para a foto de perfil, conectada com a API. Verificar configurações após ajustar envio de POST para dados de perfil. */}
+
           <button
             aria-label="Alterar foto do perfil"
             className="photo profile__photo_overlay"
@@ -65,16 +78,15 @@ function Main() {
           ></button>
         </div>
         <div className="infos profile__infos">
-          <h1 className="name infos__name">Vanessa</h1>
+          {/*o "nome" e "sobre" são obtidos do contexto CurrentUserContext*/}
+          <h1 className="name infos__name">{currentUser.name}</h1>
           <button
             aria-label="Alterar informações do perfil"
             className="edit-btn infos__edit-btn"
             type="button"
             onClick={() => handleOpenPopup(editProfilePopup)}
           ></button>
-          <h2 className="about infos__about">
-            Desenvolvedora web (full-stack)
-          </h2>
+          <h2 className="about infos__about">{currentUser.about}</h2>
         </div>
         <button
           aria-label="Adicionar novo cartão"
