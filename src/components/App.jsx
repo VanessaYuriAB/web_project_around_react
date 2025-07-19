9d2df47c9f300943dbd69212cd8edf82a4764780
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-19 05:12:00 -0300

    refactor: move cards state, API logic and handlers from Main to App - elevates the state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,16 @@ import myApi from '../utils/api.js';
 
 import CurrentUserContext from '../contexts/CurrentUserContext.js';
 
+/*
+Linha comentada para prevenir duplicação ao enviar cards iniciais. Executar apenas uma vez, quando necessário enviar os dados para a API.
+// envia meus cards iniciais
+// myApi.createInitialCards();
+*/
+
 function App() {
+  // Estado para armazenar os cartões do usuário
+  const [cards, setCards] = useState([]);
+
   // Estado para armazenar as informações do usuário atual
   const [currentUser, setCurrentUser] = useState({});
 
@@ -24,6 +33,18 @@ function App() {
       })
       .catch((err) => {
         console.error(`Erro ao obter informações do usuário: ${err}`);
+      });
+  }, []);
+
+  // Obtém os cartões iniciais do servidor quando o componente é montado e atualiza o estado dos cartões com os dados retornados pela API
+  useEffect(() => {
+    myApi
+      .getInitialCards()
+      .then((initialCards) => {
+        setCards(initialCards);
+      })
+      .catch((err) => {
+        console.error(`Erro ao obter os cartões iniciais: ${err}`);
       });
   }, []);
 
@@ -43,6 +64,41 @@ function App() {
     }));
     return updatedAvatarData; // devolve os dados para quem chamou a função (handleSubmit no componente EditAvatar)
   };
+
+  // Função para lidar com o evento de curtir/descurtir um cartão: ela recebe o cartão atual como argumento e verifica se ele já foi curtido ou não, se o cartão já foi curtido, a função envia uma solicitação para a API para remover o like, caso contrário, envia uma solicitação para adicionar o like - após a solicitação, atualiza o estado dos cartões com os dados retornados pela API
+  async function handleCardLike(card) {
+    // Verifica, mais um vez, se o cartão já foi curtido - é verificado no componente Card, mas é uma boa prática verificar novamente aqui
+    const isLiked = card.isLiked;
+
+    // Envia uma solicitação para a API e obtém os dados do cartão atualizados
+    await myApi
+      // !isLiked = ação inversa do estado atual de curtida - corresponde à shouldLike no método toggleLikeCard
+      .toggleLikeCard(card._id, !isLiked)
+      .then((updatedCard) => {
+        setCards((stateCards) =>
+          stateCards.map((currentCardInMap) =>
+            currentCardInMap._id === card._id ? updatedCard : currentCardInMap
+          )
+        );
+      })
+      .catch((error) =>
+        console.error(`Erro ao curtir/descurtir o cartão: ${error}`)
+      );
+  }
+
+  // Função para lidar com a exclusão de um cartão: ela recebe o cartão atual como argumento e envia uma solicitação para a API para excluir o cartão, após a solicitação, atualiza o estado dos cartões removendo o cartão excluído
+  async function handleCardDelete(card) {
+    await myApi
+      .deleteCard(card._id)
+      .then(() => {
+        setCards((stateCards) =>
+          stateCards.filter(
+            (currentCardInFilter) => currentCardInFilter._id !== card._id
+          )
+        );
+      })
+      .catch((error) => console.error(`Erro ao excluir o cartão: ${error}`));
+  }
 
   // Função para abrir o popup atual
   const handleOpenPopup = (popup) => {
@@ -65,6 +121,9 @@ function App() {
           popup={popup}
           onOpenPopup={handleOpenPopup}
           onClosePopup={handleClosePopup}
+          cards={cards}
+          onCardLike={handleCardLike}
+          onCardDelete={handleCardDelete}
         />
         <Footer />
       </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 
 import Popup from './components/Popup/Popup';
 
@@ -8,33 +8,18 @@ import EditAvatar from './components/Popup/components/EditAvatar/EditAvatar';
 
 import Card from './components/Card/Card';
 
-import myApi from '../../utils/api.js';
-
 import CurrentUserContext from '../../contexts/CurrentUserContext.js';
 
-/*
-Linha comentada para prevenir duplicação ao enviar cards iniciais. Executar apenas uma vez, quando necessário enviar os dados para a API.
-// envia meus cards iniciais
-// myApi.createInitialCards();
-*/
-
-function Main({ popup, onOpenPopup, onClosePopup }) {
-  const [cards, setCards] = useState([]);
-
+function Main({
+  popup,
+  onOpenPopup,
+  onClosePopup,
+  cards,
+  onCardLike,
+  onCardDelete,
+}) {
   // Obtém o usuário atual do contexto: assina o contexto CurrentUserContext
   const { currentUser } = useContext(CurrentUserContext);
-
-  // Obtém os cartões iniciais do servidor quando o componente é montado e atualiza o estado dos cartões com os dados retornados pela API
-  useEffect(() => {
-    myApi
-      .getInitialCards()
-      .then((initialCards) => {
-        setCards(initialCards);
-      })
-      .catch((err) => {
-        console.error(`Erro ao obter os cartões iniciais: ${err}`);
-      });
-  }, []);
 
   const newCardPopup = {
     children: <NewCard />,
@@ -47,41 +32,6 @@ function Main({ popup, onOpenPopup, onClosePopup }) {
   const editAvatarPopup = {
     children: <EditAvatar handleClosePopup={onClosePopup} />,
   };
-
-  // Função para lidar com o evento de curtir/descurtir um cartão: ela recebe o cartão atual como argumento e verifica se ele já foi curtido ou não, se o cartão já foi curtido, a função envia uma solicitação para a API para remover o like, caso contrário, envia uma solicitação para adicionar o like - após a solicitação, atualiza o estado dos cartões com os dados retornados pela API
-  async function handleCardLike(card) {
-    // Verifica, mais um vez, se o cartão já foi curtido - é verificado no componente Card, mas é uma boa prática verificar novamente aqui
-    const isLiked = card.isLiked;
-
-    // Envia uma solicitação para a API e obtém os dados do cartão atualizados
-    await myApi
-      // !isLiked = ação inversa do estado atual de curtida - corresponde à shouldLike no método toggleLikeCard
-      .toggleLikeCard(card._id, !isLiked)
-      .then((updatedCard) => {
-        setCards((stateCards) =>
-          stateCards.map((currentCardInMap) =>
-            currentCardInMap._id === card._id ? updatedCard : currentCardInMap
-          )
-        );
-      })
-      .catch((error) =>
-        console.error(`Erro ao curtir/descurtir o cartão: ${error}`)
-      );
-  }
-
-  // Função para lidar com a exclusão de um cartão: ela recebe o cartão atual como argumento e envia uma solicitação para a API para excluir o cartão, após a solicitação, atualiza o estado dos cartões removendo o cartão excluído
-  async function handleCardDelete(card) {
-    await myApi
-      .deleteCard(card._id)
-      .then(() => {
-        setCards((stateCards) =>
-          stateCards.filter(
-            (currentCardInFilter) => currentCardInFilter._id !== card._id
-          )
-        );
-      })
-      .catch((error) => console.error(`Erro ao excluir o cartão: ${error}`));
-  }
 
   return (
     <main className="content page__content">
@@ -126,8 +76,8 @@ function Main({ popup, onOpenPopup, onClosePopup }) {
               key={card._id}
               card={card}
               handleOpenPopup={onOpenPopup}
-              onCardLike={handleCardLike}
-              onCardDelete={handleCardDelete}
+              onCardLike={onCardLike}
+              onCardDelete={onCardDelete}
             />
           ))}
         </ul>
