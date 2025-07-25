e3a8fa9d8d84e77d2cedad3ea7ebb968a9a74f03
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-07-25 09:33:06 -0300

    refactor(style): adjust close button margins for specific popups

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -2,8 +2,15 @@ export default function Popup(props) {
   //children é o conteúdo de popup
   const { children, onClose } = props;
 
-  // Para verificar se children é o componente ImagePopup e configurar a classe do botão de fechar
+  // Para verificar se children é o componente popup que precisa de configuração diferente da classe do botão de fechar para ajuste da margem superior
+  // ImagePopup
   const isImagePopup = children.type.name === 'ImagePopup';
+
+  // DeleteConfirmation
+  const isDeleteConfirmation = children.type.name === 'DeleteConfirmation';
+
+  // EditAvatar
+  const isEditAvatar = children.type.name === 'EditAvatar';
 
   return (
     <div className="popup">
@@ -11,6 +18,8 @@ export default function Popup(props) {
       <button
         className={`icon-close-btn popup__icon-close-btn ${
           isImagePopup ? 'popup__icon-close-btn_card' : ''
+        } ${isDeleteConfirmation ? 'popup__icon-close-btn_trash' : ''} ${
+          isEditAvatar ? 'popup__icon-close-btn_photo' : ''
         }`}
         type="button"
         aria-label="Botão de fechar"
