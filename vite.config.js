732a49bb353d8d7e8613cc02e4768a619c023046
VanessaYuriAB <emaildavanessayuri@gmail.com>
2025-08-01 09:04:41 -0300

    chore: configure base path and build options in vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
+  base: '/web_project_around_react/', // 👈 Caminho correto para GitHub Pages / Já configurado, mas não é utilizado no npm run dev, apenas para npm run build e npm run preview
+  build: {
+    outDir: 'dist', // onde o Vite colocará os arquivos após o build (padrão já é "dist")
+    assetsDir: 'assets', //subpasta onde vão os arquivos estáticos (JS, CSS, imagens etc.)
+  },
 });
